a77b4b873ef82b1132703a61097c4132fa269d37
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2021-08-10 17:39:04 +0100

    add flag to control generation of OJS import xml

--- convert.py
+++ convert.py
@@ -8,11 +8,13 @@ if __name__ == '__main__':
     parser.add_argument('input_filename', help='the filename to process')
     parser.add_argument('--journal', help='the journal title being processed, e.g. scenario, boolean, alphaville', default='none')
     parser.add_argument('--type', help='the type of item being processed, either journal or conference', default='journal')
+    parser.add_argument('--ojs', help='whether to attemp to scrape legacy content and generate OJS import XML, true if yes', default='false')
     args = vars(parser.parse_args())
     print(args)
     input_file = args['input_filename']
     journal = args['journal']
     type = args['type']
+    ojs = args['ojs']
     
     if type == "journal":
         dspace_job = xmet.DspaceJob(input_file)
@@ -21,16 +23,16 @@ if __name__ == '__main__':
         crossref_job = xmet.CrossRefJournalJob(input_file)
         crossref_job.generate()
 
-        if journal == "scenario":
+        if journal == "scenario" and ojs == "true":
             ojs_job = xmet.OjsScenarioJob(input_file)
             ojs_job.generate()
-        elif journal == "boolean":
+        elif journal == "boolean" and ojs == "true":
             ojs_job = xmet.OjsBooleanJob(input_file)
             ojs_job.generate()
-        elif journal == "chimera":
+        elif journal == "chimera" and ojs == "true":
             ojs_job = xmet.OjsChimeraJob(input_file)
             ojs_job.generate()
-        elif journal == "ijpp":
+        elif journal == "ijpp" and ojs == "true":
             ojs_job = xmet.OjsIjppJob(input_file)
             ojs_job.generate()
     elif type == "conference":
